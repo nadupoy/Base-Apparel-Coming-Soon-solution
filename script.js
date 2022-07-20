acc6a26c0d74fd7ef0f504ceb596dee36f0429dc
Nadupoy <gnsampao@gmail.com>
2022-07-20 12:41:21 +0300

    Reverted files.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function verify() {
-    let email = document.getElementById("email-address").value;
-    if (email == "") {
-        document.getElementById("error-icon").style.display = "inline";
-        document.getElementById("error-message").style.display = "inline-block";
-        return false;
+    const userEmail = document.getElementById("email-address").value;
+
+    /*
+    Using the constructor function provides runtime compilation of the regular expression. Use the constructor function when you know the regular expression pattern will be changing, or you don't know the pattern and are getting it from another source, such as user input.
+    - MDN WEB DOCS
+    */
+
+    // EMAIL ADDRESS FORMAT: name@domain
+
+    const regex = new RegExp(/[-.\w]+@([\w-]+\.)+[\w-]+/g);
+
+    /*
+    Removed the quotes as the output was always 'false' even if the email format was correct.
+    - STACK OVERFLOW
+    */
+
+    let emailVerification = regex.test(userEmail);
+
+    if (emailVerification === false) {
+      document.getElementById("error-message").innerHTML = "Please provide a valid email";
     }
 }
